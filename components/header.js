68d2e8cb8705744409577f5336b2843c50aab3f8
Greg Rickaby <greg@gregrickaby.com>
2020-04-04 09:21:54 -0500

    refactor header

--- components/header.js
+++ components/header.js
@@ -1,33 +1,44 @@
+import Head from 'next/head';
+
 const Header = () => (
-  <header>
-    <img src="logo.png" alt="Greg Rickaby Development logo" />
-    <h1 className="site-title">Greg Rickaby</h1>
-    <p className="site-description">
-      Director of Engineering
-      {' '}
-      <a href="https://webdevstudios.com">@WebDevStudios</a>
-      {' '}
-      / Author & Editor
-      {' '}
-      <a href="https://www.wiley.com/en-us">@Wiley</a>
-    </p>
+  <>
+    <Head>
+      <title>Greg Rickaby</title>
+      <link rel="icon" href="/favicon.ico" />
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
 
-    <style jsx>
-      {`
-        header {
-          text-align: center;
-        }
+    <header>
+      <img src="logo.png" alt="Greg Rickaby Development logo" />
+      <h1 className="site-title">Greg Rickaby</h1>
+      <p className="site-description">
+        Director of Engineering
+        {' '}
+        <a href="https://webdevstudios.com">@WebDevStudios</a>
+        {' '}
+        / Author & Editor
+        {' '}
+        <a href="https://www.wiley.com/en-us">@Wiley</a>
+      </p>
 
-        img {
-          width: 96px;
-        }
+      <style jsx>
+        {`
+          header {
+            text-align: center;
+          }
 
-        .site-description {
-          font-size: 16px;
-        }
-      `}
-    </style>
-  </header>
+          img {
+            width: 96px;
+          }
+
+          .site-description {
+            font-size: 16px;
+          }
+        `}
+      </style>
+    </header>
+  </>
 );
 
 export default Header;
