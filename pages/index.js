d39efc8ea6d60df72cd75ba4ca9d66b016439f2a
Greg Rickaby <greg@gregrickaby.com>
2020-04-04 16:10:20 -0500

    i cannot use <Layout> inside an MDX file :(

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,58 @@
 import Layout from '../components/layout'
 
-const Index = () => <Layout />
+const Index = () => (
+  <Layout>
+    <img src="/rickaby-family-fall-2019.jpg" alt="Our family" />
+    <p>
+      Hello! I’m the Director of Engineering at{' '}
+      <a href="https://webdevstudios.com/">WebDevStudios</a> , where{' '}
+      <strong>
+        we build amazing digital platforms for enterprise level corporations
+        with WordPress
+      </strong>{' '}
+      . While at WebDevStudios, my engineers and I have solved complex website
+      problems for clients like Microsoft, Viacom, News Corporation, Campbell’s
+      Soup Company, and the NBA.
+    </p>
+    <p>
+      Over the years,{' '}
+      <strong>
+        I’ve also contributed to <a href="https://wordpress.org">WordPress</a>{' '}
+        Core
+      </strong>{' '}
+      , various WordPress themes, dozens of open source projects, including
+      <strong>
+        <a href="https://gatsbyjs.org">GatsbyJS</a>
+      </strong>{' '}
+      ,{' '}
+      <strong>
+        <a href="https://nextjs.org">Next.JS</a>
+      </strong>{' '}
+      , and <strong>have spoken at a few WordCamps</strong>.
+    </p>
+    <p>
+      I’ve also contracted with
+      <a href="https://www.wiley.com/en-us/">Wiley</a> and{' '}
+      <strong>authored a children’s book</strong>
+      about building your first website (
+      <a href="https://gregslink.co/create-a-website-am">Amazon</a> ), and{' '}
+      <strong>served as Technical Editor on two other books</strong> ,{' '}
+      <em>WordPress All-In-One For Dummies, 4E</em> ({' '}
+      <a href="https://amzn.to/37MMDLp">Amazon</a> ) and{' '}
+      <em>Professional WordPress Plugin Development, 2E</em> which will be
+      published in this spring.
+    </p>
+    <p>
+      <strong>I’m always trying to learn something new</strong> , everything
+      from leadership skills, or mastering new technology, to playing music.
+      Right now, I’m all in photography. When I’m not at work?{' '}
+      <strong>I enjoy being a dad and hanging out with family</strong> , as well
+      as serving my community through{' '}
+      <a href="https://wiregrasschurch.org">Wiregrass Church</a> . I’ve also
+      been known to moonlight as a pizzaiolo.
+    </p>
+    <p>Thanks for reading and feel free to contact me greg@gregrickaby.com</p>
+  </Layout>
+)
 
 export default Index
