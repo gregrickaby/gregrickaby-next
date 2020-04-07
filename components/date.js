6dc40e07b152b383ae1e00040e35cd30ff90b65b
Greg Rickaby <greg@gregrickaby.com>
2020-04-06 20:18:04 -0500

    add posted on date

---
+++ components/date.js
@@ -0,0 +1,17 @@
+import {parseISO, format} from 'date-fns'
+import PropTypes from 'prop-types'
+
+const Date = ({dateString}) => {
+  const date = parseISO(dateString)
+  return <time dateTime={dateString}>{format(date, 'LLLL	d, yyyy')}</time>
+}
+
+Date.propTypes = {
+  dateString: PropTypes.string
+}
+
+Date.defaultProps = {
+  dateString: ''
+}
+
+export default Date
--- pages/p/[id].js
+++ pages/p/[id].js
@@ -3,24 +3,29 @@ import fetch from 'isomorphic-unfetch'
 import Link from 'next/link'
 import {FaArrowAltCircleLeft} from 'react-icons/fa'
 import createDangerousMarkup from '../../helpers/dangerous'
+import Date from '../../components/date'
 
-const Post = (props) => (
-  <Layout>
-    <h1
-      dangerouslySetInnerHTML={createDangerousMarkup(props.data.title.rendered)}
-    />
-    <time>Published on {props.data.date}</time>
-    <p
-      dangerouslySetInnerHTML={createDangerousMarkup(
-        props.data.content.rendered
-      )}
-    />
-    <FaArrowAltCircleLeft size="16px" />{' '}
-    <Link href="/">
-      <a>Back home</a>
-    </Link>
-  </Layout>
-)
+const Post = (props) => {
+  return (
+    <Layout>
+      <h1
+        dangerouslySetInnerHTML={createDangerousMarkup(
+          props.data.title.rendered
+        )}
+      />
+      Posted on <Date dateString={props.data.date} /> by {props.data.author}
+      <p
+        dangerouslySetInnerHTML={createDangerousMarkup(
+          props.data.content.rendered
+        )}
+      />
+      <FaArrowAltCircleLeft size="16px" />{' '}
+      <Link href="/">
+        <a>Back home</a>
+      </Link>
+    </Layout>
+  )
+}
 
 Post.getInitialProps = async function (context) {
   const {id} = context.query
