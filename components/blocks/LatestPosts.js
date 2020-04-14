d7dcfa390236a5e2953b16462e594ab7243fad33
Greg Rickaby <greg@gregrickaby.com>
2020-04-14 12:05:54 -0500

    use card component and display 3 posts

--- components/blocks/LatestPosts.js
+++ components/blocks/LatestPosts.js
@@ -1,18 +1,37 @@
 import Link from 'next/link'
+import PropTypes from 'prop-types'
+import Card from './Card'
 
 const LatestPosts = ({posts}) => (
-  <section>
-    <h3>Latest from the blog</h3>
-    <ul>
-      {posts.map((post) => (
-        <li key={post.id}>
+  <section className="container px-5 py-24 mx-auto">
+    <h1 className="sm:text-3xl text-2xl title-font font-medium text-gray-900 mb-4">
+      Latest Posts
+    </h1>
+    <div className=" flex flex-wrap">
+      {/* Loop through and display 3 posts */}
+      {posts.slice(0, 3).map((post) => (
+        <div className="w-full sm:w-1/2 md:w-1/3 mb-4" key={post.id}>
           <Link href="/blog/[...all]" as={`/blog/${post.id}/${post.slug}`}>
-            <a dangerouslySetInnerHTML={{__html: post.title.rendered}} />
+            <a>
+              <Card
+                thumbnail="https://picsum.photos/300/150"
+                title={post.title.rendered}
+                text={post.excerpt.rendered}
+              />
+            </a>
           </Link>
-        </li>
+        </div>
       ))}
-    </ul>
+    </div>
   </section>
 )
+
+LatestPosts.propTypes = {
+  posts: PropTypes.array
+}
+
+LatestPosts.defaultProps = {
+  posts: []
+}
 
 export default LatestPosts
