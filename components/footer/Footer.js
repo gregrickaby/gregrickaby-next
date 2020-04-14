81ec78930cb9fc4c66028f8a9d928692b85effc3
Greg Rickaby <greg@gregrickaby.com>
2020-04-14 12:18:58 -0500

    formatting

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -4,7 +4,6 @@ import StayConnected from '../blocks/StayConnected'
 const Footer = () => (
   <footer className="text-gray-700 body-font">
     <StayConnected />
-
     <section className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
       <Logo />
       <p className="text-sm text-gray-700 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
