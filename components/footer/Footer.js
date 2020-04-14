e9ecb14d8a915da9d2a163037fa1d05ea03b9cba
Greg Rickaby <greg@gregrickaby.com>
2020-04-14 12:18:40 -0500

    add footer content

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,23 +1,32 @@
-import {FaLinkedin, FaTwitterSquare, FaGithubSquare} from 'react-icons/fa'
+import Logo from '../common/Logo'
+import StayConnected from '../blocks/StayConnected'
 
 const Footer = () => (
-  <footer>
-    <div className="social-icons">
-      <a href="https://twitter.com/webdevstudios" aria-label="twitter">
-        <FaTwitterSquare size="36px" />
-      </a>
-      <a href="https://github.com/webdevstudios" aria-label="github">
-        <FaGithubSquare size="36px" />
-      </a>
-      <a href="https://www.linkedin.com/in/webdevstudios" aria-label="linkedin">
-        <FaLinkedin size="36px" />
-      </a>
-    </div>
+  <footer className="text-gray-700 body-font">
+    <StayConnected />
 
-    <div>
-      Powered by <a href="https://nextjs.org">Next.JS</a> and hosted on{' '}
-      <a href="https://zeit.co/">Now</a>.
-    </div>
+    <section className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+      <Logo />
+      <p className="text-sm text-gray-700 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
+        Proudly powered by <a href="https://wordpress.org">WordPress</a> and{' '}
+        <a href="https://nextjs.org">NextJS</a>. Hosted by{' '}
+        <a href="https://wpengine.com">WP Engine</a>. WebDevStudios is a{' '}
+        <a href="https://wd3.co/">
+          <strong>
+            WD<span style={{color: '#ff00f0'}}>3</span> Party
+          </strong>
+        </a>
+      </p>
+      <p className="text-sm text-gray-700 inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+        <a className="mr-1" href="mailto:contact@webdevstudios.com">
+          contact@webdevstudios
+        </a>{' '}
+        /{' '}
+        <a className="ml-1" href="tel:1-855-932-3380">
+          (855) 932-3380
+        </a>
+      </p>
+    </section>
   </footer>
 )
 
