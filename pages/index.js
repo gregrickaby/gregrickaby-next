0266638b292e4501fcfb60103f7ffae24a2ce891
Greg Rickaby <greg@gregrickaby.com>
2020-04-14 10:20:20 -0500

    use two column block

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,59 @@
+import Button from '../components/common/Buttons'
+import Hero from '../components/blocks/Hero'
 import Layout from '../components/common/Layout'
 import Link from 'next/link'
 import PropTypes from 'prop-types'
+import TwoColumn from '../components/blocks/TwoColumn'
 import fetch from 'isomorphic-unfetch'
-import Hero from '../components/blocks/Hero'
 
 const Homepage = ({posts}) => (
   <Layout>
     <section>
-      <Hero image={require('../public/images/wdscamp.jpg?resize&size=728')} />
+      <Hero image={require('../public/wdscamp.jpg?resize&size=1920')} />
+      <TwoColumn
+        column1={<img src={require('../public/640x480.png')} />}
+        column2={
+          <>
+            <h2 className="sm:text-3xl text-2xl title-font font-medium text-gray-900 mb-4">
+              This is column number 1
+            </h2>
+            <p className="leading-relaxed mb-8">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ex quo,
+              id quod omnes expetunt, beate vivendi ratio inveniri et comparari
+              potest. Immo videri fortasse. Sed haec ab Antiocho, familiari
+              nostro, dicuntur multo melius et fortius, quam a Stasea
+              dicebantur. Sed quanta sit alias, nunc tantum possitne esse tanta.
+              Quaero igitur, quo modo hae tantae commendationes a natura
+              profectae subito a sapientia relictae sint. Duo Reges: constructio
+              interrete. Unum est sine dolore esse, alterum cum voluptate. Sed
+              haec quidem liberius ab eo dicuntur et saepius.{' '}
+            </p>
+            <Button text="Learn More" url="/" />
+          </>
+        }
+      />
+      <TwoColumn
+        column1={
+          <>
+            <h2 className="sm:text-3xl text-2xl title-font font-medium text-gray-900 mb-4">
+              This is column number 2
+            </h2>
+            <p className="leading-relaxed mb-8">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ex quo,
+              id quod omnes expetunt, beate vivendi ratio inveniri et comparari
+              potest. Immo videri fortasse. Sed haec ab Antiocho, familiari
+              nostro, dicuntur multo melius et fortius, quam a Stasea
+              dicebantur. Sed quanta sit alias, nunc tantum possitne esse tanta.
+              Quaero igitur, quo modo hae tantae commendationes a natura
+              profectae subito a sapientia relictae sint. Duo Reges: constructio
+              interrete. Unum est sine dolore esse, alterum cum voluptate. Sed
+              haec quidem liberius ab eo dicuntur et saepius.{' '}
+            </p>
+            <Button text="Learn More" url="/" />
+          </>
+        }
+        column2={<img src={require('../public/640x480.png')} />}
+      />
       <aside className="latest-posts">
         <h3>Latest from the blog</h3>
         <ul>
