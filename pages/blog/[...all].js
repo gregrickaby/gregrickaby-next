4f2a53cdaa88697d1e082ae8abd1095488a848d0
Greg Rickaby <greg@gregrickaby.com>
2020-04-16 14:24:18 -0500

    use new components

--- pages/blog/[...all].js
+++ pages/blog/[...all].js
@@ -1,3 +1,5 @@
+import Categories from '../../components/blog/Categories'
+import Tags from '../../components/blog/Tags'
 import Layout from '../../components/common/Layout'
 import Link from 'next/link'
 import PostDate from '../../components/blog/PostDate'
@@ -6,31 +8,35 @@ import config from '../../lib/config'
 import fetch from 'isomorphic-unfetch'
 import {FaArrowAltCircleLeft} from 'react-icons/fa'
 
-const Post = ({post}) => (
-  <Layout>
-    <article
-      className="max-w-3xl mx-auto p-4 text-lg"
-      style={{fontFamily: 'Libre Franklin,sans-serif'}}
-    >
-      <h1
-        className="text-4xl font-bold mb-4"
-        dangerouslySetInnerHTML={{__html: post.title.rendered}}
-      />
-      <span className="text-sm italic">
-        Posted on <PostDate dateString={post.date} />
-      </span>
-      <div
-        className="post my-4 font-normal"
-        dangerouslySetInnerHTML={{__html: post.content.rendered}}
-      />
-      <Link href="/">
-        <a className="flex">
-          <FaArrowAltCircleLeft size="18px" className="mr-2" /> Go Back
-        </a>
-      </Link>
-    </article>
-  </Layout>
-)
+const Post = ({post}) => {
+  return (
+    <Layout>
+      <article
+        className="max-w-3xl mx-auto p-4 text-lg"
+        style={{fontFamily: 'Libre Franklin,sans-serif'}}
+      >
+        <h1
+          className="text-4xl font-bold mb-4"
+          dangerouslySetInnerHTML={{__html: post.title.rendered}}
+        />
+        <span className="text-sm italic">
+          Posted on <PostDate dateString={post.date} />
+        </span>
+        <Categories terms={post.categories} />
+        <div
+          className="post my-4 font-normal"
+          dangerouslySetInnerHTML={{__html: post.content.rendered}}
+        />
+        <Tags terms={post.tags} />
+        <Link href="/">
+          <a className="flex">
+            <FaArrowAltCircleLeft size="18px" className="mr-2" /> Go Back
+          </a>
+        </Link>
+      </article>
+    </Layout>
+  )
+}
 
 export async function getServerSideProps(context) {
   const id = context.query.all[0]
