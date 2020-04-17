5fd1bed0ea7e1861171f4c2a7fb30ac654ab28fa
Greg Rickaby <greg@gregrickaby.com>
2020-04-17 09:43:36 -0500

    display posts on tag archive

--- pages/blog/tags/[...id].js
+++ pages/blog/tags/[...id].js
@@ -2,15 +2,36 @@ import Hero from '../../../components/blog/Hero'
 import Layout from '../../../components/common/Layout'
 import Link from 'next/link'
 import PropTypes from 'prop-types'
-import Tags from '../../../components/blog/Tags'
+import Card from '../../../components/blocks/Card'
 import config from '../../../lib/config'
 import fetch from 'isomorphic-unfetch'
 import {FaArrowAltCircleLeft} from 'react-icons/fa'
 
-const Archive = ({post}) => {
+const TagArchive = ({posts}) => {
   return (
     <Layout>
-      <pre>{JSON.stringify(post, null, 2)}</pre>
+      <section className="container px-5 py-24 mx-auto">
+        <div className="flex flex-wrap">
+          {posts.map((post) => (
+            <div className="w-full sm:w-1/2 md:w-1/3 mb-4" key={post.id}>
+              <Link
+                href="/blog/post/[...id]"
+                as={`/blog/post/${post.slug}/${post.id}`}
+              >
+                <a>
+                  <Card post={post} />
+                </a>
+              </Link>
+            </div>
+          ))}
+        </div>
+
+        <Link href="/">
+          <a className="flex">
+            <FaArrowAltCircleLeft size="18px" className="mr-2" /> Go Back
+          </a>
+        </Link>
+      </section>
     </Layout>
   )
 }
@@ -18,21 +39,21 @@ const Archive = ({post}) => {
 export async function getServerSideProps(context) {
   const id = context.query.id[0]
   const res = await fetch(`${config.apiUrl}posts?tags=${id}`)
-  const post = await res.json()
+  const posts = await res.json()
 
   return {
     props: {
-      post
+      posts
     }
   }
 }
 
-Archive.propTypes = {
-  post: PropTypes.object
+TagArchive.propTypes = {
+  posts: PropTypes.object
 }
 
-Archive.defaultProps = {
-  post: []
+TagArchive.defaultProps = {
+  posts: []
 }
 
-export default Archive
+export default TagArchive
