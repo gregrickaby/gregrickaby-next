eb01122e7395792e7b3c85803f2afea40519a30f
Greg Rickaby <greg@gregrickaby.com>
2020-04-22 14:40:37 -0500

    fix eslint errors

--- components/common/Buttons.js
+++ components/common/Buttons.js
@@ -1,9 +1,16 @@
+import PropTypes from 'prop-types'
+
 export default function Button(props) {
   return (
     <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">
       <a href={props.url}>{props.text}</a>
     </button>
   )
+}
+
+Button.propTypes = {
+  url: PropTypes.string,
+  text: PropTypes.string
 }
 
 export function Pill(props) {
@@ -14,10 +21,20 @@ export function Pill(props) {
   )
 }
 
+Pill.propTypes = {
+  url: PropTypes.string,
+  text: PropTypes.string
+}
+
 export function Outline(props) {
   return (
     <button className="bg-transparent hover:bg-orange-500 text-orange-700 font-semibold hover:text-white py-2 px-4 border border-orange-500 hover:border-transparent rounded">
       <a href={props.url}>{props.text}</a>
     </button>
   )
+}
+
+Outline.propTypes = {
+  url: PropTypes.string,
+  text: PropTypes.string
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
+import PropTypes from 'prop-types'
 import '../styles/index.css'
 
 const App = ({Component, pageProps}) => <Component {...pageProps} />
+
+App.propTypes = {
+  Component: PropTypes.is.required,
+  pageProps: PropTypes.is.required
+}
 
 export default App
