a9b32b6942fc52d21abe3303ff73114b9309fc0f
Greg Rickaby <greg@gregrickaby.com>
2020-04-23 15:27:57 -0500

    use next/link for prefetching

--- components/common/Buttons.js
+++ components/common/Buttons.js
@@ -1,9 +1,11 @@
 import PropTypes from 'prop-types'
-
+import Link from 'next/link'
 export default function Button(props) {
   return (
     <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">
-      <a href={props.url}>{props.text}</a>
+      <Link href={props.url}>
+        <a>{props.text}</a>
+      </Link>
     </button>
   )
 }
@@ -16,7 +18,9 @@ Button.propTypes = {
 export function Pill(props) {
   return (
     <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded-full">
-      <a href={props.url}>{props.text}</a>
+      <Link href={props.url}>
+        <a>{props.text}</a>
+      </Link>
     </button>
   )
 }
@@ -29,7 +33,9 @@ Pill.propTypes = {
 export function Outline(props) {
   return (
     <button className="bg-transparent hover:bg-orange-500 text-orange-700 font-semibold hover:text-white py-2 px-4 border border-orange-500 hover:border-transparent rounded">
-      <a href={props.url}>{props.text}</a>
+      <Link href={props.url}>
+        <a>{props.text}</a>
+      </Link>
     </button>
   )
 }
