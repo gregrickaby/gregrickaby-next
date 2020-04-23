6264f92a4ba0697c6dbfef3b6352b8eb5311ebb3
Greg Rickaby <greg@gregrickaby.com>
2020-04-23 11:38:44 -0500

    add alert and contact pages

---
+++ pages/alert.js
@@ -0,0 +1,13 @@
+import Layout from '../components/common/Layout'
+
+const Alert = () => (
+  <Layout>
+    <section className="container px-5 py-24 mx-auto">
+      <h1 className="sm:text-3xl text-2xl title-font font-medium text-gray-900 mb-8 text-center">
+        Alert
+      </h1>
+    </section>
+  </Layout>
+)
+
+export default Alert
