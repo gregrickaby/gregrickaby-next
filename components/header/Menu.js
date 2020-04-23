f7fe211884671f07d4aede024d28966150f10611
Greg Rickaby <greg@gregrickaby.com>
2020-04-23 11:38:27 -0500

    link to contact page

--- components/header/Menu.js
+++ components/header/Menu.js
@@ -39,7 +39,7 @@ const Menu = () => (
         </ul>
       </div>
 
-      <Link href="/">
+      <Link href="/contact">
         <a>
           <button className="inline-flex items-center bg-gray-200 border-0 py-1 px-3 focus:outline-none hover:bg-gray-300 rounded text-base mt-4 md:mt-0">
             <span className="mr-1">Contact Us</span>
