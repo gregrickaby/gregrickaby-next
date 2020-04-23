da6e63f2a0a0aa684a197cfaa7a64cefba940aae
Greg Rickaby <greg@gregrickaby.com>
2020-04-23 10:30:30 -0500

    pass in props so we can customize

--- components/blocks/LatestPosts.js
+++ components/blocks/LatestPosts.js
@@ -2,13 +2,13 @@ import Link from 'next/link'
 import PropTypes from 'prop-types'
 import Card from './Card'
 
-const LatestPosts = ({posts}) => (
+const LatestPosts = (props) => (
   <section className="container px-5 py-24 mx-auto">
     <h1 className="sm:text-3xl text-2xl title-font font-medium text-gray-900 mb-4">
       Latest Posts
     </h1>
     <div className="flex flex-wrap">
-      {posts.slice(0, 3).map((post) => (
+      {props.data.slice(0, props.display).map((post) => (
         <div className="w-full sm:w-1/2 md:w-1/3 mb-4" key={post.id}>
           <Link
             href="/blog/post/[...id]"
@@ -25,11 +25,12 @@ const LatestPosts = ({posts}) => (
 )
 
 LatestPosts.propTypes = {
-  posts: PropTypes.array
+  data: PropTypes.array,
+  display: PropTypes.number
 }
 
 LatestPosts.defaultProps = {
-  posts: []
+  display: 9
 }
 
 export default LatestPosts
