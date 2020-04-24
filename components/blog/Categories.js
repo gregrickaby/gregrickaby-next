a97d9bc906caeddc80d330c4ee09e165b4594f78
Greg Rickaby <greg@gregrickaby.com>
2020-04-23 21:05:20 -0500

    refactor categories

--- components/blog/Categories.js
+++ components/blog/Categories.js
@@ -2,32 +2,27 @@ import Link from 'next/link'
 import PropTypes from 'prop-types'
 import {getTermNames} from '../../lib/api'
 
-const Categories = ({terms}) => {
-  const categories = getTermNames('categories', terms)
-
-  // Grab the first TermID.
-  const termID = terms[0]
-
-  return (
-    <>
-      {categories.map((category) => (
-        <span key={category}>
-          {' '}
-          <Link href="/blog/category/[...id]" as={`/blog/category/${termID}`}>
-            <a>{category} </a>
-          </Link>
-        </span>
-      ))}
-    </>
-  )
-}
+const Categories = (props) => (
+  <>
+    {props.categories.length > 0 && (
+      <div className="inline-block">
+        {props.categories.map((id) => {
+          let categoryName = getTermNames('categories', id)
+          return (
+            <span key={id}>
+              <Link href="/blog/category/[...id]" as={`/blog/category/${id}`}>
+                <a>{categoryName} </a>
+              </Link>
+            </span>
+          )
+        })}
+      </div>
+    )}
+  </>
+)
 
 Categories.propTypes = {
-  terms: PropTypes.array
-}
-
-Categories.defaultProps = {
-  terms: []
+  categories: PropTypes.array
 }
 
 export default Categories
