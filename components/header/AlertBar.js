863dbccfefb42f39ae79966a375a4572cd5c50b5
Greg Rickaby <greg@gregrickaby.com>
2020-04-26 08:00:40 -0500

    mobile fixes

--- components/header/AlertBar.js
+++ components/header/AlertBar.js
@@ -3,11 +3,11 @@ import {FaInfoCircle} from 'react-icons/fa'
 
 const AlertBar = () => (
   <div
-    className="flex flex-col lg:flex-row items-center justify-center bg-orange-500 text-white font-medium px-4 py-3"
+    className="flex flex-col lg:flex-row items-center text-center justify-center bg-orange-500 text-white font-medium px-4 py-3"
     role="alert"
   >
     <FaInfoCircle size="18px" />
-    <p className="ml-2 mr-4">
+    <p className="ml-2 mr-4 py-4">
       Something happened that you should know about, so we placed a message up
       here.
     </p>
