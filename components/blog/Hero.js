9d695d3f4da4ecf22a8c743ba9b65eac48e06626
Greg Rickaby <greg@gregrickaby.com>
2020-04-27 08:38:02 -0500

    stylelint fix

--- components/blog/Hero.js
+++ components/blog/Hero.js
@@ -8,7 +8,7 @@ const Hero = ({post}) => (
     className="mb-16 text-white body-font text-center bg-cover bg-no-repeat"
     style={{backgroundImage: `url(${post.acf.blog_hero_image.url})`}}
   >
-    <div style={{backgroundColor: 'rgba(0,0,0,0.7)'}}>
+    <div style={{backgroundColor: 'rgba(0, 0, 0, 0.7)'}}>
       <div className="container flex flex-col items-center mx-auto lg:py-48 lg:px-64 py-24 px-32 mx-auto">
         <h1
           className="title-font md:text-5xl sm:text-4xl text-3xl mb-8 font-bold"
