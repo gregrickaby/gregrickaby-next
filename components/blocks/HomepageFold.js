96ee9918b599c723d00cbfb33fc77b3beba81e26
Greg Rickaby <greg@gregrickaby.com>
2020-05-04 21:51:38 -0500

    display block data

--- components/blocks/HomepageFold.js
+++ components/blocks/HomepageFold.js
@@ -1,9 +1,21 @@
 import PropTypes from 'prop-types'
+import {arrayToObject} from '../../lib/api'
 
 const HomepageFold = (props) => {
+  // Convert the Array of Objects.
+  const blockObject = arrayToObject(
+    props.data.acf.content_blocks,
+    'acf_fc_layout'
+  )
+
+  // Grab the block data.
+  const blockData = blockObject['homepage_fold']
+
   return (
     <div>
-      <pre>{JSON.stringify(props.data, null, 2)}</pre>
+      <pre>
+        <pre>{JSON.stringify(blockData, null, 2)}</pre>
+      </pre>
     </div>
   )
 }
