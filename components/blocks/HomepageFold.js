0d5cab5ec535b3391775e63a453ab6d81efbe729
Greg Rickaby <greg@gregrickaby.com>
2020-05-05 21:14:05 -0500

    start styling homepage fold hero

--- components/blocks/HomepageFold.js
+++ components/blocks/HomepageFold.js
@@ -1,22 +1,30 @@
+import Button from '../common/Button'
+import Link from 'next/link'
 import PropTypes from 'prop-types'
-import {arrayToObject} from '../../lib/api'
+import {getBlockData} from '../../lib/api'
 
 const HomepageFold = (props) => {
-  // Convert the Array of Objects.
-  const blockObject = arrayToObject(
-    props.data.acf.content_blocks,
-    'acf_fc_layout'
-  )
-
-  // Grab the block data.
-  const blockData = blockObject['homepage_fold']
+  const blockData = getBlockData(props.data.acf.content_blocks, 'homepage_fold')
 
   return (
-    <div>
-      <pre>
-        <pre>{JSON.stringify(blockData, null, 2)}</pre>
-      </pre>
-    </div>
+    <section
+      className="mb-16 bg-no-repeat bg-bottom text-white text-center"
+      style={{
+        backgroundImage: `url(${blockData.background_media.url})`,
+        minHeight: `800px`
+      }}
+    >
+      <div className="container flex flex-col items-center text-black mx-auto py-12 px-16 lg:py-24 lg:px-32">
+        <h1 className="pt-7 text-4xl">
+          {blockData.block_options.section_title}
+        </h1>
+        <div dangerouslySetInnerHTML={{__html: blockData.description}} />
+        <Button text={blockData.button_text} url="/contact" />
+        <Link href="/services">
+          <a>{blockData.secondary_cta_text}</a>
+        </Link>
+      </div>
+    </section>
   )
 }
 
