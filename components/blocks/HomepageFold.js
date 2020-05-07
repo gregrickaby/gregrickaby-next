8c6c3142dc4e3ff19ad38ee2568c3ae6edff0f4c
Greg Rickaby <greg@gregrickaby.com>
2020-05-06 20:22:00 -0500

    finish homepage fold

--- components/blocks/HomepageFold.js
+++ components/blocks/HomepageFold.js
@@ -11,18 +11,26 @@ const HomepageFold = (props) => {
       className="mb-16 bg-no-repeat bg-bottom text-white text-center"
       style={{
         backgroundImage: `url(${blockData.background_media.url})`,
-        minHeight: `800px`
+        backgroundSize: `100%`
       }}
     >
-      <div className="container flex flex-col items-center text-black mx-auto py-12 px-16 lg:py-24 lg:px-32">
-        <h1 className="pt-7 text-4xl">
+      <div
+        className="container flex flex-col items-center max-w-6xl text-black mx-auto pt-12 px-16 overflow-x-hidden"
+        style={{paddingBottom: '30%'}}
+      >
+        <h1 className="my-8 text-5xl">
           {blockData.block_options.section_title}
         </h1>
-        <div dangerouslySetInnerHTML={{__html: blockData.description}} />
-        <Button text={blockData.button_text} url="/contact" />
-        <Link href="/services">
-          <a>{blockData.secondary_cta_text}</a>
-        </Link>
+        <div
+          className="text-xl"
+          dangerouslySetInnerHTML={{__html: blockData.description}}
+        />
+        <div className="w-auto my-8">
+          <Button text={blockData.button_text} url="/contact" />
+          <Link href="/services">
+            <a className="ml-2 underline">{blockData.secondary_cta_text}</a>
+          </Link>
+        </div>
       </div>
     </section>
   )
