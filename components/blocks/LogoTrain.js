fbe0642924278dd266e4178fa8d8e330a49dfdf9
Greg Rickaby <greg@gregrickaby.com>
2020-05-09 08:57:22 -0500

    display logos

--- components/blocks/LogoTrain.js
+++ components/blocks/LogoTrain.js
@@ -1,9 +1,9 @@
-import Button from '../common/Button'
-import Link from 'next/link'
 import PropTypes from 'prop-types'
 import {getBlockData} from '../../lib/api'
+import {getLogoTrainFeaturedMedia} from '../../lib/api'
 
 const LogoTrain = (props) => {
+  // Get the block data for the Logo Train.
   const blockData = getBlockData(props.data.acf.content_blocks, 'logo_train')
 
   return (
@@ -11,9 +11,15 @@ const LogoTrain = (props) => {
       style={{backgroundColor: `${blockData.block_options.background_color}`}}
     >
       <h3>{blockData.block_options.section_title}</h3>
-      {blockData.logos.map((logo) => (
-        <p key={logo.id}>{logo.post_title}</p>
-      ))}
+      {
+        // Map over logos...
+        blockData.logos.map((media) => {
+          // Find the logo associated with the client.
+          let logo = getLogoTrainFeaturedMedia(media.ID)
+          // Display the logo.
+          return <img key={logo.ID} src={logo.source_url} alt={logo.alt_text} />
+        })
+      }
     </section>
   )
 }
