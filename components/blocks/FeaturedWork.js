ddeb7636d869307876adfde4a6475841a45d3373
Greg Rickaby <greg@gregrickaby.com>
2020-05-13 20:09:02 -0500

    display logos and post title

--- components/blocks/FeaturedWork.js
+++ components/blocks/FeaturedWork.js
@@ -15,30 +15,28 @@ const FeaturedWork = (props) => {
       <h3 className="mb-3 text-3xl font-light uppercase text-center tracking-widest">
         {blockData.block_options.section_title}
       </h3>
-      {
-        // Map over featured_work
-        // Look up each Portfolio item's featured ID
-        // Get the media based on featured ID
-        // Display title overlay on top
-        // This is a funky masonry grid
-        // Note the hover state (background gradient fades away)
-
-        // Map over logos...
-        blockData.featured_work.map((post) => {
-          // Find the logo associated with the portfolio item.
-          let logo = getFeaturedImage('portfolio', post.ID)
-          // Display the logo.
-          return (
-            <img
-              key={post.ID}
-              className="flex-auto object-scale-down w-4"
-              src={logo.source_url}
-              alt={logo.alt_text}
-            />
-          )
-        })
-      }
-      <pre>{JSON.stringify(blockData, null, 2)}</pre>
+      <div className="grid lg:grid-cols-4">
+        {
+          // This is a funky masonry grid
+          // Display title overlay on top
+          // Note the hover state (background gradient fades away)
+          // Map over featured work...
+          blockData.featured_work.map((post) => {
+            // Find the logo associated with the portfolio item.
+            let logo = getFeaturedImage('portfolio', post.ID)
+            // Display the logo.
+            return (
+              <div
+                key={post.ID}
+                className="bg-cover bg-no-repeat"
+                style={{backgroundImage: `url(${logo.source_url})`}}
+              >
+                <div className="">{post.post_title}</div>
+              </div>
+            )
+          })
+        }
+      </div>
     </section>
   )
 }
