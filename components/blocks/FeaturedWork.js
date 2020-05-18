65990770c24472141dee40873b37ef0be2901602
Greg Rickaby <greg@gregrickaby.com>
2020-05-18 10:47:57 -0500

    display additional logo

--- components/blocks/FeaturedWork.js
+++ components/blocks/FeaturedWork.js
@@ -1,12 +1,12 @@
 import PropTypes from 'prop-types'
-import {getBlockData, getFeaturedImage} from 'lib/api'
+import {getBlockData, getFeaturedImage, GetPost} from 'lib/api'
 
 const FeaturedWork = (props) => {
   const blockData = getBlockData(props.data.acf.content_blocks, 'featured_work')
 
   return (
     <section
-      className="grid grid-cols-1 lg:grid-cols-2"
+      className=""
       style={{
         backgroundColor: `${blockData.block_options.background_color}`,
         color: `${blockData.block_options.font_color}`
@@ -15,13 +15,37 @@ const FeaturedWork = (props) => {
       <h3 className="mb-3 text-3xl font-light uppercase text-center tracking-widest">
         {blockData.block_options.section_title}
       </h3>
-      <div className="grid lg:grid-cols-4">
+      <div className="grid grid-cols-3">
         {
-          // This is a funky masonry grid
-          // Display title overlay on top
-          // Note the hover state (background gradient fades away)
           // Map over featured work...
-          blockData.featured_work.map((post) => {
+          blockData.featured_work.map((item) => {
+            // Grab the post data for the portoflio item.
+            let featuredWork = GetPost('portfolio', item.ID)
+
+            // Find the logo for the portfolio item.
+            let featuredLogo = getFeaturedImage('portfolio', item.ID)
+
+            // Display the featured work.
+            return (
+              <div
+                key={item.ID}
+                className="bg-cover bg-no-repeat"
+                style={{backgroundImage: `url(${featuredLogo.source_url})`}}
+              >
+                <div className="bg-cover bg-no-repeat">
+                  <img
+                    src={featuredWork.acf.portfolio_item_logo.url}
+                    alt={featuredWork.acf.portfolio_item_logo.title}
+                  />
+                </div>
+                <div className="bg-white text-black">{item.post_title}</div>
+              </div>
+            )
+          })
+        }
+        {/* {
+          // Map over optional featured work...
+          blockData.optional_featured_work.map((post) => {
             // Find the logo associated with the portfolio item.
             let logo = getFeaturedImage('portfolio', post.ID)
             // Display the logo.
@@ -31,11 +55,11 @@ const FeaturedWork = (props) => {
                 className="bg-cover bg-no-repeat"
                 style={{backgroundImage: `url(${logo.source_url})`}}
               >
-                <div className="">{post.post_title}</div>
+                <div className="bg-white text-black">{post.post_title}</div>
               </div>
             )
           })
-        }
+        } */}
       </div>
     </section>
   )
